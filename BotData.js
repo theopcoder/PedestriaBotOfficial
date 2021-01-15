73ba4f9201b14a93819364a7448b973d8601dca4
theopcoder <58401646+theopcoder@users.noreply.github.com>
2021-01-15 11:06:27 -0800

    Grammar Fix

--- BotData.js
+++ BotData.js
@@ -1,6 +1,6 @@
 var BotData = [
-    Version = "2.1.0",
-    VN = "2.1.0 Bot Updates",//VN stands for Version Name
+    Version = "2.1.1",
+    VN = "2.1.1 Grammar Fix",//VN stands for Version Name
     Developer = "TheMLGDude#2177 | theopcoder",
     DeveloperDiscord = "TheMLGDude#2177",
     DeveloperGitHub = "theopcoder",
